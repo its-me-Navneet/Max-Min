4bd3f2d4f6a6a1e2745b93572235f784b94704c4
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-26 21:37:35 +0530

    Added Refactor 1

--- src/App.java
+++ src/App.java
@@ -1,31 +1,33 @@
-public class App {
+public class App<T extends Comparable<T>> {
 
-     private static String maxEement(String a,String b,String c) {
-        
-        String maxOfTwo = (a.compareTo(b) >0 ? a:b) ; 
-        String maximumElement = (maxOfTwo.compareTo(c) >0 ? maxOfTwo :c) ;
-        return maximumElement ;
-     
-    }
-    public static void main(String[] args) throws Exception {
-      
+    public T findMaximum(T value1, T value2, T value3) {
+        T max = value1;
 
-          System.out.println("Program to find max of 3 over differnt data types");
-        
-          String a="Apple";
-          String b="App";
-          String c="Aaat" ;
-        String maxTC1 = maxEement(a,b,c) ; 
-        System.out.println(maxTC1);
-        String  maxTC2 = maxEement(a,c,b) ; 
-        System.out.println(maxTC2);
-        String maxTC3 = maxEement(c,b,a) ; 
-        System.out.println(maxTC3);
-    
-       
-   
+        if (value2.compareTo(max) > 0) {
+            max = value2;
+        }
 
+        if (value3.compareTo(max) > 0) {
+            max = value3;
+        }
+
+        return max;
     }
 
-   
+    public static void main(String[] args) {
+        App<Integer> integerFinder = new App<>();
+        System.out.println("Maximum Integer: " + integerFinder.findMaximum(5, 8, 3));
+        System.out.println("Maximum Integer: " + integerFinder.findMaximum(8, 5, 3));
+        System.out.println("Maximum Integer: " + integerFinder.findMaximum(5, 3, 8));
+
+        App<Double> doubleFinder = new App<>();
+        System.out.println("Maximum Double: " + doubleFinder.findMaximum(3.14, 2.5, 5.7));
+        System.out.println("Maximum Double: " + doubleFinder.findMaximum(3.14, 5.7, 3.14));
+        System.out.println("Maximum Double: " + doubleFinder.findMaximum(5.7, 2.5, 3.14));
+
+        App<String> stringFinder = new App<>();
+        System.out.println("Maximum String: " + stringFinder.findMaximum("apple", "banana", "orange"));
+        System.out.println("Maximum String: " + stringFinder.findMaximum("apple", "orange", "bnanana"));
+        System.out.println("Maximum String: " + stringFinder.findMaximum("orange", "banana", "apple"));
+    }
 }
