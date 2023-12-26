dd9616e1fa7770fdca87ae8009bccd0e8bf447ad
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-26 18:55:54 +0530

    Added UC2

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
 public class App {
 
-     private static int maxEement(Integer a,Integer b,Integer c) {
+     private static float maxEement(float a,float b,float c) {
         
-        Integer maxOfTwo = (a.compareTo(b)==-1 ? b:a) ;
-        int maximumElement = (maxOfTwo.compareTo(c) == -1 ? c:maxOfTwo) ;
+        float maxOfTwo = (a>b ? a:b) ;
+        float maximumElement = (maxOfTwo > c? maxOfTwo :c) ;
         return maximumElement ;
      
     }
@@ -12,14 +12,14 @@ public class App {
 
           System.out.println("Program to find max of 3 over differnt data types");
         
-          Integer a=10 ;
-          Integer b=20 ;
-          Integer c=30 ;
-        int maxTC1 = maxEement(a,b,c) ; 
+          float a=10.20f ;
+          float b=20.5f ;
+          float c=30.5f ;
+        float maxTC1 = maxEement(a,b,c) ; 
         System.out.println(maxTC1);
-        int maxTC2 = maxEement(a,c,b) ; 
+        float  maxTC2 = maxEement(a,c,b) ; 
         System.out.println(maxTC2);
-        int maxTC3 = maxEement(c,b,a) ; 
+        float maxTC3 = maxEement(c,b,a) ; 
         System.out.println(maxTC3);
     
        
