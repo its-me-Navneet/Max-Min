743880c3ad2ae82383d89590e5a18e73f0fd2486
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-26 22:11:25 +0530

    Added UC4

--- src/App.java
+++ src/App.java
@@ -1,69 +1,66 @@
+import java.util.Arrays;
+
 public class App<T extends Comparable<T>> { 
 
-    T a,b,c ,max ;
+    T a[],max ;
 
-    App(T a,T b,T c){
+    App(T a[]){
         this.a =a;
-        this.b=b ;
-        this.c =c;  
-        this.max = this.findMaximum(a, b, c) ;
+        
+        this.max = this.findMaximum(a) ;
     }
     App(){
 
     } 
-    public T findMaximum(T value1, T value2, T value3) {
-        T max = value1;
 
-        if (value2.compareTo(max) > 0) {
-            max = value2;
-        }
+    public T findMaximum(T []values) {
+       
+        Arrays.sort(values);
 
-        if (value3.compareTo(max) > 0) {
-            max = value3;
-        }
+        int size = values.length;
 
-        return max;
+        return values[size-1];
     }
 
     public static void main(String[] args) {  
 
         //...........   Integer ......
-        Integer a=5 ;
-        Integer b=8 ;
-        Integer c=3 ;
-        App<Integer> integerFinderTc1 = new App<>(a,b,c);  
+        Integer []a = {5,10,12} ;
+        Integer []b = {5,12,10} ;
+        Integer []c = {12,10,1} ;
+        App<Integer> integerFinderTc1 = new App<>(a);  
         System.out.println("maximum value is " + integerFinderTc1.max);
-        App<Integer> integerFinderTc2 = new App<>(b,a,c); 
+        App<Integer> integerFinderTc2 = new App<>(b); 
          System.out.println("maximum value is " + integerFinderTc2.max);
-        App<Integer> integerFinderTc3 = new App<>(a,c,b); 
+        App<Integer> integerFinderTc3 = new App<>(c); 
          System.out.println("maximum value is " + integerFinderTc3.max);
     
 
         //  .......Double......... 
 
-        Double p =5.5 ;
-        Double q= 6.5 ;
-        Double r =7.5 ;
+        Double p[] ={5.5,6.5,7.5} ;
+        Double q []= {6.5,7.5,5.5} ;
+        Double r[] ={7.5,6.5,5.5} ;
 
-          App<Double> DoubleFinderTc1 = new App<>(p,q,r);  
+          App<Double> DoubleFinderTc1 = new App<>(p);  
         System.out.println("maximum value is " + DoubleFinderTc1.max);
-        App<Double> DoubleFinderTc2 = new App<>(p,r,q); 
+        App<Double> DoubleFinderTc2 = new App<>(q); 
          System.out.println("maximum value is " + DoubleFinderTc2.max);
-        App<Double> DoubleFinderTc3 = new App<>(r,p,q); 
+        App<Double> DoubleFinderTc3 = new App<>(r); 
          System.out.println("maximum value is " + DoubleFinderTc3.max); 
 
 
          // ....string ..........
 
-         String x="Apple" ;
-         String y="App" ;
-         String z= "Appp" ;
+         String x[]={"Apple","App","Appp"} ;
+         String y[]={"App","Apple","Appp"} ;
+         String z[]={ "Appp","App","Apple"} ;
          
-          App<String> StringFinderTc1 = new App<>(x,y,z);  
+          App<String> StringFinderTc1 = new App<>(x);  
         System.out.println("maximum value is " + StringFinderTc1.max);
-        App<String> StringFinderTc2 = new App<>(x,z,y); 
+        App<String> StringFinderTc2 = new App<>(y); 
          System.out.println("maximum value is " + StringFinderTc2.max);
-        App<String> StringFinderTc3 = new App<>(z,x,y); 
+        App<String> StringFinderTc3 = new App<>(z); 
          System.out.println("maximum value is " + StringFinderTc3.max); 
 
 
